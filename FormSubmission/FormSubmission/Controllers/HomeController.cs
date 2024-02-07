f421b6ee524bfd04983de41a58ec46794ae9fab5
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2024-02-07 12:16:22 +0600

    Form validation

---
+++ FormSubmission/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿
+
+using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Person());
+        }
+        [HttpPost]
+        public ActionResult Index(Person p)
+        {
+            if (ModelState.IsValid) { //checking the rules imposed in Person class
+                return RedirectToAction("Contact");
+            }
+            return View(p);
+        }
+        //[HttpPost]
+        //public ActionResult Index(string Name, string Username, string[] Hobbies)
+        //{
+            
+
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Index(FormCollection fc) {
+        //    var name = fc["Name"];
+        //    var uname = fc["Username"];
+        //    var hobbies = fc["Hobbies"];
+
+
+        //    return View();
+        //}
+
+        public ActionResult Sample() {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FormSubmission/FormSubmission/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(7,ErrorMessage ="My Error")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(5,MinimumLength =2)]
+        public string Username { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Profession { get; set; }
+        [Required]
+        public string[] Hobbies { get; set; }
+        [Required]
+        [Range(1,40)]
+        public int Roll { get; set; }
+    }
+}
